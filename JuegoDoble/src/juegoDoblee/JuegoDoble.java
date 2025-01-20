c1338f020d62f06f44faaee87c4b65fd4fadafc4
onion122 <f.javierdiazmiranda@gmail.com>
2025-01-20 11:11:34 +0100

    Third commit, Corrected

--- JuegoDoble/src/juegoDoblee/JuegoDoble.java
+++ JuegoDoble/src/juegoDoblee/JuegoDoble.java
@@ -4,11 +4,11 @@ public class JuegoDoble {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		int [][] array= new int[4][4];
-		//int a=sc.nextInt();	
+		//int a=sc.nextInt();
 		
 		System.out.println("Introduce un número del 1 al 8.");
-		for(int i=0;i<=4;i++) {
-			for(int j=0;j<=4;j++) {
+		for(int i=0;i<array.length;i++) {
+			for(int j=0;j<array.length;j++) {
 				System.out.println("Introduce el número para la posición ["+i+"] ["+j+"]");
 				array[i][j]= sc.nextInt();
 			}
